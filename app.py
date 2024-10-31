8d78418916a3049a37e4703f61405cfcd0b2e248
kylen4592 <167936048+kylen4592@users.noreply.github.com>
2024-10-31 15:39:19 -0500

    Descriptive message about the changes

--- app.py
+++ app.py
@@ -20,6 +20,16 @@ def get_db_connection():
     #return the connection object
     return conn
 
+#function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+
+    return post
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -60,5 +70,34 @@ def create():
     return render_template('create.html')
 
 
+#create route to edit post. Load page with get or post method
+#pass the post id as url parameter 
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    #get the post from database with select query
+    post = get_post(id)
+    #determine if the page was request with GET or POST
+    if request.method== 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        #if no eror
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('content is required')
+        else:
+            conn = get_db_connection()
+
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+
+            #redir to home page
+            return redirect(url_for('index'))
+
+    #if GET then display
+    #if POST, process the form data
+    return render_template('edit.html', post=post)
 
 app.run()
